c1cc3ce8de26698f32a200f6d349ac3ff684a0ce
soheila <soheilalotfi@ymail.com>
2019-08-15 14:43:26 -0400

    create a server

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require("http");
+
+//----create a server
+var port = 8080;
+
+var server = http.creatServer(hanndleRequest);
+
+//----handleRequest function-----
+function handleRequest(request, response) {}
